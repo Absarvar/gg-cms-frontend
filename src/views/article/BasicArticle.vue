50cadf1383195512a1ed54d9cf8ae0eab0444fe7
yangmi <yangmi@it.com>
2022-04-18 22:36:36 +0800

    'fenxiaoPingzheng'

--- src/views/article/BasicArticle.vue
+++ src/views/article/BasicArticle.vue
@@ -1,53 +1,53 @@
 <template>
   <div>
 
-    <div class="text-center">
-      <table align="center" class="mx-auto table table-bordered table-hover heavy_border t2print" style="width:700px;vertical-align:middle">
-        <tbody>
-          <tr>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;" colspan="8">
-              <h2>供广深圳肉类智能交易市场产品检测报告</h2>
-              <div>购货单位</div>
-              <div>
-                <div>NO</div>
-                <div>出证日期</div>
-              </div>
-            </td>
+    <div class="text-center" style="width:500px;height:300px;" align="center" >
+      <h4>供广深圳肉类智能交易市场动物产品分销信息凭证</h4>
+      <div style="float:left;">
+        <div> &nbsp;</div>
+        购货单位：</div>
+      <div style="float:right;">
+        <div>NO：11111</div>
+        <div>出证日期：</div>
+      </div>
+      <table
+        class="table table-bordered table-hover heavy_border t2print"
+        style="vertical-align:middle;padding-top:15px;margin-bottom:1px;">
+        <tbody >
+          <tr style="">
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" rowspan="2">商品名称</td>
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" rowspan="2">生产单位</td>
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" rowspan="2">原检疫证号</td>
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" >上级供应商</td>
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" >分销凭证号</td>
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" rowspan="2">规格</td>
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" rowspan="2">数量</td>
+            <td scope="col" style="text-align:center;border-width:1px;border-color:#000000;padding:0px;vertical-align:middle;" rowspan="2">重量（千克）</td>
           </tr>
           <tr>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">商品名称</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">生成单位</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">原检疫证号</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">上级供应商</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">分销凭证号</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">规格</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">数量</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">重量（千克）</td>
+            <td colspan="2" style="border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle">已经过分销的，须填写</td>
           </tr>
           <tr>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;" ></td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">生成单位</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">原检疫证号</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">上级供应商</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">分销凭证号</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">规格</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">数量</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;">重量（千克）</td>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br></th>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br></th>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br></th>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br></th>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br><br></th>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br></th>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br></th>
+            <th style="text-align:center;border-width:1px;border-color:#000000;text-align:center;padding:0px;vertical-align:middle;"><br>    <br></th>
           </tr>
-
           <tr>
-            <td scope="col" style="text-align:left;border-width:2px;border-color:#000000;" colspan="7">合计：</td>
-            <td scope="col" style="text-align:center;border-width:2px;border-color:#000000;" colspan="1"></td>
+            <th style="border-width:1px;border-color:#000000;text-align:left;" colspan="7">合计：</th>
+            <th style="border-width:1px;border-color:#000000;">    </th>
           </tr>
         </tbody>
-
-        <div>
-          <div>供货单位</div>
-          <div>出证单位：供广深圳肉类智能交易市场</div>
-        </div>
-        <div>出证人</div>
-
       </table>
+      <div style="float:left;" align="left" >
+        <div>供货单位：</div>
+        <div>出证单位：供广深圳肉类智能交易市场</div>
+      </div>
+      <div style="float:right;">出证人：</div>
     </div>
 
   </div>
